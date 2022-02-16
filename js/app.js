3fe6bf356415328dc07c15346713621bb6bc266e
dipta3 <diptabiswas79@gmail.com>
2022-02-17 01:12:45 +0600

    expence and balacne added

--- js/app.js
+++ js/app.js
@@ -1,26 +1,52 @@
+function income() {
+
+    const incomeText = document.getElementById('income-field').value;
+    const incomeNum = parseFloat(incomeText);
+    if (typeof incomeNum == "number") {
+        if (incomeNum > 0) {
+            document.getElementById('alert-msg').innerText = "";
+            const balance = document.getElementById('balance');
+            const restMoney = incomeNum - totalExpense();
+            balance.innerText = restMoney;
+        }
+        else {
+            document.getElementById('alert-msg').innerText = "Give number greater than 0 Or Please Give A Number";
+        }
+    }
+
+}
+
+
+function totalExpense() {
+    const foodText = document.getElementById('food').value;
+    const foodNum = parseFloat(foodText);
+
+    const rentText = document.getElementById('rent').value;
+    const rentNum = parseFloat(rentText);
+
+    const clothText = document.getElementById('cloth').value;
+    const clothNum = parseFloat(clothText);
+
+    if (typeof foodNum == "number" && typeof rentNum == "number" && typeof clothNum == "number") {
+        if (foodNum > 0 && rentNum > 0 && clothNum > 0) {
+            document.getElementById('alert-msg').innerText = "";
+            const expenses = document.getElementById('total-expenses');
+            const totalExpense = foodNum + rentNum + clothNum;
+            expenses.innerText = totalExpense;
+            return totalExpense;
+
+        }
+        else {
+            document.getElementById('alert-msg').innerText = "Give number greater than 0 Or Please Give A Number";
+        }
+    }
+
+}
+
+
 document.getElementById('calculate-button').addEventListener('click', function () {
-    let totalIncome = document.getElementById('input-field').value;
-    totalIncome = parseInt(totalIncome);
-
-    let foodExpence = document.getElementById('food').value;
-    foodExpence = parseInt(foodExpence);
-
-    let rentExpence = document.getElementById('rent').value;
-    rentExpence = parseInt(rentExpence);
-
-    let clotheExpence = document.getElementById('clothe').value;
-    clotheExpence = parseInt(clotheExpence);
-    totalSpend = foodExpence + rentExpence + clotheExpence;
-
-    totalIncome = '';
-    foodExpence = '';
-
-    let totalExpense = document.getElementById('total-expenses');
-    totalExpense.innerText = totalSpend;
-
-    let balance = document.getElementById('balance');
-    balance.innerText = totalIncome - totalSpend;
-
+    income();
+    totalExpense();
 
 
 })
