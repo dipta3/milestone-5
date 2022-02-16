80bb1be1888956c9858ddeac640ad3b1144b1ced
dipta3 <diptabiswas79@gmail.com>
2022-02-16 22:31:06 +0600

    third commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-console.log('I Am JS');
+document.getElementById('calculate-button').addEventListener('click', function () {
+    let totalIncome = document.getElementById('input-field').value;
+    totalIncome = parseInt(totalIncome);
+
+    let foodExpence = document.getElementById('food').value;
+    foodExpence = parseInt(foodExpence);
+
+    let rentExpence = document.getElementById('rent').value;
+    rentExpence = parseInt(rentExpence);
+
+    let clotheExpence = document.getElementById('clothe').value;
+    clotheExpence = parseInt(clotheExpence);
+    totalSpend = foodExpence + rentExpence + clotheExpence;
+
+    totalIncome = '';
+    foodExpence = '';
+
+    let totalExpense = document.getElementById('total-expenses');
+    totalExpense.innerText = totalSpend;
+
+    let balance = document.getElementById('balance');
+    balance.innerText = totalIncome - totalSpend;
+
+
+
+})
